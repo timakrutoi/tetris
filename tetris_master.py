4190e149253bbf90fdf1a2953781a18e6f6c86cc
Timofei Oleinik <timaoleinik@gmail.com>
2023-09-15 14:53:44 +0300

    Update NN arch

--- tetris_master.py
+++ tetris_master.py
@@ -5,8 +5,89 @@ import torch.nn.functional as F
 import random
 
 
+class GlobalPool2d(nn.Module):
+    def __init__(self):
+        super(GlobalPool2d, self).__init__()
+
+    def forward(self, x):
+        b, c, w, h = x.shape
+        return nn.functional.adaptive_avg_pool2d(x, 1).reshape((b, c, 1, 1))
+
+
+class TetrisMaster2(nn.Module):
+    def __init__(self, depth=2, bw=10, bh=20, n_hidden=25):
+        super().__init__()
+        
+        self.max_tetrimino = 6
+        self.act = nn.ReLU
+
+        # unet like arch
+        self.unet_down = nn.Sequential()
+        self.unet_up = nn.Sequential()
+        self.mid = nn.Sequential()
+
+        for i in range(1, depth):
+            # conv + pool + act
+            self.unet_down.append(nn.Sequential(
+                nn.Conv2d(i**2, (i+1)**2, (2,2), stride=1, padding=1),
+                nn.MaxPool2d(2),
+                self.act()
+            ))
+
+        for i in range(depth, 1, -1):
+            # convtransposed + pool + act
+            self.unet_up.append(nn.Sequential(
+                nn.ConvTranspose2d(i**2, (i-1)**2, (2,2), stride=2, padding=0),
+                self.act()
+            ))
+
+        if depth:
+            self.mid.append(nn.Linear(1, 100))
+            self.mid.append(self.act())
+            self.mid.append(nn.Linear(100, 1000))
+            self.mid.append(self.act())
+            self.mid.append(nn.Linear(1000, depth**2))
+            self.mid.append(self.act())
+
+        self.pos = nn.Sequential(
+            nn.Linear(bw * bh, bw),
+            self.act()
+        )
+        self.rot = nn.Sequential(
+            nn.Linear(bw * bh, 4),
+            self.act()
+        )
+
+        for _ in range(n_hidden):
+            self.pos.append(nn.Linear(bw, bw))
+            self.pos.append(self.act())
+            self.rot.append(nn.Linear(4, 4))
+            self.rot.append(self.act())
+
+        self.pos.append(nn.Softmax(dim=0))
+        self.rot.append(nn.Softmax(dim=0))
+
+    def forward(self, b, n):
+        d = [b.unsqueeze(1)]
+        for s in self.unet_down:
+            d.append(s(d[-1]))
+
+        mid = self.mid(n / self.max_tetrimino)
+        d[-1] = d[-1] + mid.unsqueeze(2).unsqueeze(3).expand(*d[-1].shape)
+
+        # going up with skip connections
+        b = d[-1]
+        for i, s in enumerate(self.unet_up):
+            b = s(b) + d[-i - 2]
+
+        p = self.pos(b.view(-1))
+        r = self.rot(b.view(-1))
+
+        return p, r
+
+
 class TetrisMaster(nn.Module):
-	def __init__(self, n_layers=2, bw=10, bh=20, c_hidden=32):
+	def __init__(self, n_layers=5, bw=10, bh=20, c_hidden=32):
 		super(TetrisMaster, self).__init__()
 		self.eps = 0.9
 		self.bw = bw
