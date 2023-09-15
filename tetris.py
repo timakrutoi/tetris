d6c4510a5b341b55d14b06a001f7de9ca01e22c8
Timofei Oleinik <timaoleinik@gmail.com>
2023-09-15 14:52:48 +0300

    Fix game

--- tetris.py
+++ tetris.py
@@ -2,50 +2,60 @@ import numpy as np
 import torch
 
 
+# colored and not colored
+c = 1
+n = 0
+
 tetriminos = {
     'L': np.array([
-            [0, 0, 1],
-            [1, 1, 1]
+            [n, n, c],
+            [c, c, c]
         ]),
     'J': np.array([
-            [1, 1, 1],
-            [0, 0, 1]
+            [c, c, c],
+            [n, n, c]
         ]),
     'S': np.array([
-            [0, 1, 1],
-            [1, 1, 0]
+            [n, c, c],
+            [c, c, n]
         ]),
     'Z': np.array([
-            [1, 1, 0],
-            [0, 1, 1]
+            [c, c, n],
+            [n, c, c]
         ]),
     'square': np.array([
-            [1, 1],
-            [1, 1]
+            [c, c],
+            [c, c]
         ]),
     'bar': np.array([
-            [1, 1, 1, 1]
+            [c, c, c, c]
         ]),
 }
 
 # standart reward
-s_r = 0.001
+s_r = 0.0001
 
 rewards = {
-    'line_clear_reward': 15 * s_r,
-    'height_penalty': -1 * s_r,
-    'incorrect_place_penalty': -3 * s_r,
-    'lose_game_penalty': -10 * s_r,
+    'line_clear_reward': 30 * s_r,
+    'hight_reward': 1 * s_r,
+    'success_place_reward': 1 * s_r,
+    'repeate_pen': -1 * s_r,
+    'incorrect_place_penalty': -1 * s_r,
+    'lose_game_penalty': -300 * s_r,
 }
 
 class Tetris:
-    def __init__(self, w=10, h=20):
+    def __init__(self, w=10, h=20, rewards=rewards):
         self.width = w
         self.height = h
 
         self.board = np.zeros((self.height, self.width))
         self.buffer = np.zeros((self.height, self.width))
         self.next = 5
+        
+        self.last_turn = None
+        self.rewards = rewards
+        self.total_lines_burnt = 0
 
     def clear_board(self, make_ft=False):
         self.board = np.zeros((self.height, self.width))
@@ -68,25 +78,37 @@ class Tetris:
         # move down
         h, w = cur_tetrimino.shape
 
-        # print(col, self.height - np.argmax(self.board[:, col]))
-        if h - max(self.board[:, col] * range(self.height)) > self.height:
-            return rewards['lose_game_penalty'], rewards['lose_game_penalty']
-
         max_h = get_height(self.board)
-        for i in range(0, self.height - h + 1):
+        prev_board = self.board.copy()
+        for i in range(h, self.height + 1):
             next_board = self.board.copy()
+
+#             print(get_height(self.board) + h, self.height)
+            if get_height(self.board) + h > self.height:
+                return self.rewards['lose_game_penalty'], self.rewards['lose_game_penalty']
+
             try:
+                i -= h
                 next_board[i:i+h, col:col+w] += cur_tetrimino
             except ValueError:
-                return rewards['incorrect_place_penalty'], rewards['incorrect_place_penalty']
-
+                if i < h:
+                    return self.rewards['incorrect_place_penalty'], self.rewards['incorrect_place_penalty']
+                else:
+                    break
             if np.sum(next_board > 1):
                 break
-            self.buffer = next_board.copy()
-        i = self.height - i
+            prev_board = next_board.copy()
+        self.board = prev_board.copy()
 
-        f = rewards['height_penalty'] if (i - h) >= max_h else -rewards['height_penalty']
-        r = 10*(max_h / self.height) * rewards['height_penalty']
+        r, f = 0, 0
+
+#         print(self.height - i + 1, i-1, h, max_h)
+        if (self.height - i + 1) <= max_h:
+            r += self.rewards['hight_reward']
+            f += self.rewards['hight_reward']
+
+        r += self.rewards['success_place_reward']
+        f += self.rewards['success_place_reward']
 
         return r, f
 
@@ -94,7 +116,7 @@ class Tetris:
         # check complete lines
         r = 0
         i, j, = 0, 0
-        lines = np.sum(self.buffer, 1)
+        lines = np.sum(self.board, 1)
         if np.sum(lines == self.width):
             next_board = np.zeros(self.board.shape)
             i, j = 0, 0
@@ -103,15 +125,17 @@ class Tetris:
                     break
                 if lines[-i-1] == self.width:
                     j+=1
-                    r += 1
+                    r+=1
 
-                next_board[-i-1] = self.buffer[-j-1]
+                next_board[-i-1] = self.board[-j-1]
                 i+=1
                 j+=1
-            self.buffer = next_board
-        return r * rewards['line_clear_reward']
+            self.board = next_board
+        self.total_lines_burnt += r
+#         print(r)
+        return r**2 * self.rewards['line_clear_reward']
 
-    def turn(self, col, rot, update=True):
+    def turn(self, col, rot):
         rp, rr = 0, 0
         # rotate piece
         cur_tetrimino = np.rot90(list(tetriminos.values())[self.next], rot)
@@ -124,15 +148,18 @@ class Tetris:
         rp += r
         rr += r
         # print(f'2 {rp, rr=}')
-        if update:
-            # update board and next piece
-            self.board = self.buffer.copy()
-            self.next = np.random.randint(0, len(tetriminos))
+        self.next = np.random.randint(0, len(tetriminos))
+
+        if self.last_turn is not None:
+            rp += self.rewards['repeate_pen'] if self.last_turn[0] == col else 0
+            rr += self.rewards['repeate_pen'] if self.last_turn[1] == rot else 0
+        self.last_turn = (col, rot)
 
         return rp, rr
 
     def print(self):
         print(list(tetriminos.values())[self.next])
+        print(f'{self.total_lines_burnt=}')
         print()
         b = []
         for idx, i in enumerate(self.board):
@@ -143,14 +170,18 @@ class Tetris:
 
 
 def get_height(buffer):
-    hs = [i for i in range(buffer.shape[1]) if np.sum(buffer[-i-1])]
-    return np.argmax(hs) if len(hs) > 0 else 0
+    hs = [(np.argmax(buffer[:,i])) for i in range(buffer.shape[1])]
+    hs = np.array(hs)
+    if np.sum(hs) == 0:
+        return 0
+    hs[hs==0] = buffer.shape[0]
+    return buffer.shape[0] - np.min(hs)
 
 if __name__ == '__main__':
     t = Tetris()
 
+    t.set_random_state()
     while True:
-        t.set_random_state()
         t.print()
         col, rot = input('input col and rot: ').split(' ')
         r = t.turn(int(col), int(rot))
