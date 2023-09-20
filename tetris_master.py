8eba4b783f3cc70ae6f6b69075d184b6e6afc008
Timofei Oleinik <timaoleinik@gmail.com>
2023-09-20 16:50:51 +0300

    Update NN

--- tetris_master.py
+++ tetris_master.py
@@ -5,19 +5,10 @@ import torch.nn.functional as F
 import random
 
 
-class GlobalPool2d(nn.Module):
-    def __init__(self):
-        super(GlobalPool2d, self).__init__()
-
-    def forward(self, x):
-        b, c, w, h = x.shape
-        return nn.functional.adaptive_avg_pool2d(x, 1).reshape((b, c, 1, 1))
-
-
 class TetrisMaster2(nn.Module):
-    def __init__(self, depth=2, bw=10, bh=20, n_hidden=25):
+    def __init__(self, depth=2, bw=10, bh=20, n_hidden=256):
         super().__init__()
-        
+
         self.max_tetrimino = 6
         self.act = nn.ReLU
 
@@ -49,31 +40,28 @@ class TetrisMaster2(nn.Module):
             self.mid.append(nn.Linear(1000, depth**2))
             self.mid.append(self.act())
 
-        self.pos = nn.Sequential(
-            nn.Linear(bw * bh, bw),
-            self.act()
-        )
-        self.rot = nn.Sequential(
-            nn.Linear(bw * bh, 4),
-            self.act()
+        self.out = nn.Sequential(
+            nn.Linear(bw * bh, 100),
+            self.act(),
         )
 
         for _ in range(n_hidden):
-            self.pos.append(nn.Linear(bw, bw))
-            self.pos.append(self.act())
-            self.rot.append(nn.Linear(4, 4))
-            self.rot.append(self.act())
+            self.out.append(nn.Linear(100, 100))
+            self.out.append(self.act())
 
-        self.pos.append(nn.Softmax(dim=0))
-        self.rot.append(nn.Softmax(dim=0))
+        self.out.append(nn.Linear(100, 4))
+        self.out.append(nn.Softmax(dim=0))
 
     def forward(self, b, n):
         d = [b.unsqueeze(0).unsqueeze(0)]
+        # print(d[-1].shape)
         for s in self.unet_down:
             d.append(s(d[-1]))
 
         mid = self.mid(n / self.max_tetrimino)
         mid = mid.unsqueeze(2).unsqueeze(3)
+        
+        #print(f'{d[-1].shape} + {mid.shape}')
         d[-1] = d[-1] + mid.expand(*d[-1].shape)
 
         # going up with skip connections
@@ -81,67 +69,17 @@ class TetrisMaster2(nn.Module):
         for i, s in enumerate(self.unet_up):
             b = s(b) + d[-i - 2]
 
-        p = self.pos(b.view(-1))
-        r = self.rot(b.view(-1))
+        b = b.view(-1)
+        o = self.out(b)
 
-        return p, r
-
-
-class TetrisMaster(nn.Module):
-	def __init__(self, n_layers=5, bw=10, bh=20, c_hidden=32):
-		super(TetrisMaster, self).__init__()
-		self.eps = 0.9
-		self.bw = bw
-		self.bh = bh
-		kernel = (2, 2)
-
-		self.start = nn.Linear(1, c_hidden)
-		self.np = nn.Linear(c_hidden, c_hidden)
-		self.pipeline = nn.Sequential()
-
-		self.pipeline.append(nn.Conv2d(1, c_hidden, kernel, stride=1, padding=1))
-
-		for i in range(n_layers):
-			self.pipeline.append(nn.Conv2d(c_hidden, c_hidden, kernel, stride=1, padding=1))
-
-		self.pool = nn.MaxPool2d(2)
-		self.act = nn.ReLU()
-		# 6 coz its quantity of tetriminos
-		self.end1 = nn.Linear(96*2, bw)
-		self.end2 = nn.Linear(96*2, 4)
-		# self.pipeline.append(nn.ReLU())
-		# self.pipeline.append(nn.Softmax(dim=0))
-
-	def forward(self, b, n):
-		x = b
-		n = self.start(n)
-		for m in self.pipeline:
-			x1 = self.pool(m(x))
-			# x1 = m(x)
-			x2 = self.np(n)[0]
-			x2 = x2.view(-1, 1, 1).expand(*x1.shape)
-			# print(f'{x1.shape=}, {x2.shape=}')
-			x = self.act(x1 + x2)
-
-		x1, x2 = self.end1(x.view(-1)), self.end2(x.view(-1))
-		# print(f'{x1.shape=} {x2.shape=}')
-		x = torch.cat((x1, x2), dim=0)
-		return x
-
-	def select_action(self):
-		y = self.forward(x)
-		col = y[:w]
-		rot = y[w:]
-
-		if random.random() > self.eps:
-			col = random.randint(0, self.bw)
-			rot = random.randint(0, 4)
-			return torch.cat(F.one_hot(col, self.bw), F.one_hot(rot, 4))
-		
-		return torch.cat(F.one_hot(col, self.bw), F.one_hot(rot, 4))
+        return o
 
 
 if __name__ == '__main__':
-	pass
+    model = TetrisMaster2()
+    print(model)
 
+    y = model(torch.rand((10, 20)), torch.tensor([1]).view(-1, 1))
+    print(y)
+    y.sum().backward()
 
